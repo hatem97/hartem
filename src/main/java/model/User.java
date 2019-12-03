44eb3eb9b15075668a3ea743d9df3ec24d8d7945
Artem contrader <a.matyushin@contrader.it>
2019-12-03 15:56:42 +0100

    porcazzo3

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,40 +1,12 @@
 package model;
 
+import javax.persistence.Entity;
+
+@Entity(name = "User")
 public class User {
     private String nome;
     private String cognome;
 
 
-    public User(String nome, String cognome) {
-        super();
-            this.nome = nome;
-            this.cognome = cognome;
-    }
 
-    public User() {
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCognome() {
-        return cognome;
-    }
-
-    public void setCognome(String cognome) {
-        this.cognome = cognome;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "nome='" + nome + '\'' +
-                ", cognome='" + cognome + '\'' +
-                '}';
-    }
 }
