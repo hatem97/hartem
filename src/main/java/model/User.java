29cb689424346a86a64713dec2f8e8e094989cb9
Artem contrader <a.matyushin@contrader.it>
2019-12-03 16:01:11 +0100

    porcazzo4

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,12 +1,22 @@
 package model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity(name = "User")
 public class User {
+    @Id
+    private Integer id;
     private String nome;
     private String cognome;
 
 
+    public User(Integer id, String nome, String cognome) {
+        this.id = id;
+        this.nome = nome;
+        this.cognome = cognome;
+    }
 
+    public User() {
+    }
 }
