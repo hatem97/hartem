961c5fc03a290ab1a5c7e37e0aa24ba1beb03d29
hatem97 <rellikrow@gmail.com>
2019-12-03 16:38:23 +0100

    libro

--- src/main/java/service/LibroService.java
+++ src/main/java/service/LibroService.java
@@ -1,11 +1,14 @@
 package service;
+import model.Libro;
+import dao.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LibroService {
     @Autowired
-private Libro
+    private LibroRepository repository;
+
 public Libro getLibro(Integer id){return repository.findOne(id);}
 public void addLibro(Libro libro){repository.save(libro);}
 public Libro findAll(Libro libro){return findAll(libro);}
